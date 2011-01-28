a9ea1dd6a9207a8c47687767d4013bc4407fce56
sjacoby <sam@shackmanpress.com>
2011-01-27 22:50:59 -0500

    Implementing models.

--- mycms/entries/admin.py
+++ mycms/entries/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+import django.forms as forms
 from entries.models import Image
 
 admin.site.register(Image)
--- mycms/entries/models.py
+++ mycms/entries/models.py
@@ -2,16 +2,57 @@ from django.db import models
 import os
 
 
-
 class Image( models.Model ):
+
+    def __unicode__( self ):
+        return self.title
 
     title = models.CharField( max_length=100 )
     src = models.ImageField( upload_to='images' )
 
 class Entry( models.Model ):
 
-    order = models.IntegerField( 'Order', blank=True, null=True )
+    images = models.ManyToManyField( Image, through='EntryRelationship', blank=True )
+    order = models.PostiiveIntegerField( 'Order', blank=True, null=True )
     display = models.BooleanField( default=1 )
 
     class Meta:
         abstract = True
+        ordering = ['-order']
+        verbose_name_plural = 'Entries'
+
+class Book ( Entry ):
+
+    title = models.CharField( max_length=100 )
+    subtitle = models.CharField( max_length=100, blank=True )
+    content = models.TextField( blank=True )
+    sidebar = models.TextField( blank=True )
+
+    class Meta( Entry.Meta ):
+        verbose_name_plural = 'Books'
+
+class Typography ( Entry ):
+    
+    title = models.CharField( max_length=100 )
+    subtitle = models.CharField( max_length=100, blank=True )
+    content = models.TextField( blank=True )
+    sidebar = models.TextField( blank=True )
+    
+    class Meta( Entry.Meta ):
+
+class News ( Entry ):
+
+    date = models.DateField( auto_now_add=True )
+    content = models.TextField( blank=True )
+    
+    class Meta( Entry.Meta ):
+
+class EntryRelationship(models.Model):
+    
+    image = models.ForeignKey(Image)
+    entry = models.ForeignKey(Entry)
+    cover = models.BooleanField( default=0 )
+
+    def __unicode__( self ):
+        return 'Related Images'
+
