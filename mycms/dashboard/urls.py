152f4abe45678d61cf94a1063a39ec03e35bad0c
Sam Jacoby <sjacoby@orville.local>
2011-06-12 16:20:43 -0400

    Expanding dashboard to have re-orderable items

--- mycms/dashboard/urls.py
+++ mycms/dashboard/urls.py
@@ -4,5 +4,6 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('dashboard.views',
     # Request index.html when nothing else is listed
     url(r'^$', 'index', name="dashboard_index"),
+    url(r'^action/$', 'action')
 )
 
--- mycms/dashboard/views.py
+++ mycms/dashboard/views.py
@@ -1,8 +1,16 @@
 # Create your views here.
 import logging
 
+from django.db.models import F
+from django.http import HttpResponse
 from django.views.generic.simple import direct_to_template
+from django.shortcuts import redirect, get_object_or_404
+from django.core.urlresolvers import reverse
+
+
 from mycms.stamps import models
+
+log = logging.getLogger(__name__)
 
 def index(request):
 
@@ -13,3 +21,35 @@ def index(request):
     print items
 
     return direct_to_template(request, 'stamps/list.html', context)
+
+def action(request):
+    if not request.POST:
+        log.error("Did not receive POST with this request; redirect")
+        return redirect(reverse('dashboard_index')) 
+    
+    item_id = request.POST['id']
+    order = int(request.POST['order']) + 1 # We use 1-indexing
+
+    stamp = get_object_or_404(models.Stamp, pk=item_id)
+    original_order = stamp.order
+
+    
+    if order < original_order:
+        middle = models.Stamp.objects.filter(family=stamp.family, order__gte=order, order__lt=original_order)
+        if middle is not None:
+            middle.update(order=F('order') + 1)
+        msg = "Updated ordering"
+    elif order > original_order:
+        middle = models.Stamp.objects.filter(family=stamp.family, order__gt=original_order, order__lte=order)
+        if middle is not None:
+            middle.update(order=F('order') - 1)
+        msg = "Updated ordering"
+    else:
+        log.info("Order remains unchanged; redirect")
+        msg = "Updated. Order unchanged"
+         
+    stamp.order = order
+    stamp.save()
+
+    return HttpResponse(msg)
+
