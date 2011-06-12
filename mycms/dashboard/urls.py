36a859c658745cdc4b589b326e77fe0835456fbd
Sam Jacoby <sjacoby@orville.local>
2011-06-12 12:10:34 -0400

    Rudimentary dashboard w/o any editing capabilities.

---
+++ mycms/dashboard/urls.py
@@ -0,0 +1,8 @@
+from django.conf import settings
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('dashboard.views',
+    # Request index.html when nothing else is listed
+    url(r'^$', 'index', name="dashboard_index"),
+)
+
--- mycms/dashboard/views.py
+++ mycms/dashboard/views.py
@@ -1,1 +1,15 @@
 # Create your views here.
+import logging
+
+from django.views.generic.simple import direct_to_template
+from mycms.stamps import models
+
+def index(request):
+
+    context = {}
+
+    items  = models.Designer.cobjects.list().select_related()
+    context['items'] = items
+    print items
+
+    return direct_to_template(request, 'stamps/list.html', context)
