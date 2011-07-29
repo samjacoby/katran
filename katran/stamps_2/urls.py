714f22b2b0f0e629ee48900df53aee7497c3c277
Sam Jacoby <sam@shackmanpress.com>
2011-07-29 07:53:08 -0400

    Removing backed-up files

--- katran/stamps_2/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import *
-urlpatterns = patterns('stamps.views',
-        (r'^$', 'index'),
-        (r'^(?P<designer_name>\w+)/$', 'stamp'),
-        (r'^(?P<designer_name>\w+)/(?P<stamp_family_id>\d+)/$', 'stamp'),
-        (r'^(?P<designer_name>\w+)/(?P<stamp_family_id>\d+)/(?P<stamp_id>\d+)/$',
-            'stamp'),
-        )
